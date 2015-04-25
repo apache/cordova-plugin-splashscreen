00e8999eeb03291baef71b5e1b1ce380e12f0449
sgrebnov <v-segreb@microsoft.com>
2015-04-25 14:56:39 -0700

    CB-8750 [wp8]: Rewrite resoultion helper

--- src/wp/ResolutionHelper.cs
+++ src/wp/ResolutionHelper.cs
@@ -21,39 +21,18 @@ namespace WPCordovaClassLib.Cordova.Commands
     public enum Resolutions { WVGA, WXGA, HD };
 
     public static class ResolutionHelper
-    {
-        private static bool IsWvga
+    { 
+       public static Resolutions CurrentResolution
         {
             get
             {
-                return Application.Current.Host.Content.ScaleFactor == 100;
-            }
-        }
-
-        private static bool IsWxga
-        {
-            get
-            {
-                return Application.Current.Host.Content.ScaleFactor == 160;
-            }
-        }
-
-        private static bool IsHD
-        {
-            get
-            {
-                return Application.Current.Host.Content.ScaleFactor == 150;
-            }
-        }
-
-        public static Resolutions CurrentResolution
-        {
-            get
-            {
-                if (IsWvga) return Resolutions.WVGA;
-                else if (IsWxga) return Resolutions.WXGA;
-                else if (IsHD) return Resolutions.HD;
-                else throw new InvalidOperationException("Unknown resolution");
+                switch (Application.Current.Host.Content.ScaleFactor) 
+                {
+                    case 100: return Resolutions.WVGA;
+                    case 160: return Resolutions.WXGA;
+                    case 150: return Resolutions.HD;
+                }
+                throw new InvalidOperationException("Unknown resolution");
             }
         }
     }
